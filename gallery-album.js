2a55b4da66ec5410a4a850389e2e90e53a36c218
Mikey <218mikey@gmail.com>
2025-11-13 19:31:39 -0500

    Update

--- gallery-album.js
+++ gallery-album.js
@@ -52,12 +52,12 @@ const photoAlbums = {
     'commercial-1': {
         title: 'Industrial Concrete Installation',
         photos: [
-            'https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?w=1200&h=900&fit=crop',
-            'https://images.unsplash.com/photo-1541888946425-d81bb19240f5?w=1200&h=900&fit=crop',
-            'https://images.unsplash.com/photo-1581092160562-40aa08e78837?w=1200&h=900&fit=crop',
-            'https://images.unsplash.com/photo-1504307651254-35680f356dfd?w=1200&h=900&fit=crop',
-            'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?w=1200&h=900&fit=crop',
-            'https://images.unsplash.com/photo-1600607687939-ce8a6c25118c?w=1200&h=900&fit=crop'
+            'https://tinyurl.com/ymuk54sy',
+            'https://tinyurl.com/5786xx96',
+            'https://tinyurl.com/2hew4rtz',
+            'https://tinyurl.com/5a5ajcmz',
+            'https://tinyurl.com/yr2uptkt',
+            'https://tinyurl.com/mw4xtxej'
         ]
     },
     'repair-1': {
